39634aa3e1caef32c1052740a9d1c2d64ef448b8
SYJ_MSI <tom990422@gmail.com>
2025-01-08 08:49:49 +0900

    🚧

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./*.html"],
   theme: {
@@ -23,6 +25,13 @@ module.exports = {
         xl: '1280px',
         '2xl': '1600px', // 2xl 추가
         '3xl': '1800px', // 3xl 추가
+        /* *
+         * sd (720 x 480)
+         * hd (1280 x 720)
+         * fhd (1920 x 1080)
+         * qhd (2560 x 1440)
+         * uhd (3840 x 2160)
+         */
       },
       fontSize: (() => {
         const fontSize = {};
@@ -31,10 +40,18 @@ module.exports = {
         }
         return fontSize;
       })(),
+      textShadow: {
+        sm: '1px 1px 2px rgba(0, 0, 0, 0.6)',
+        md: '2px 2px 4px rgba(0, 0, 0, 0.6)',
+      },
     },
     fontFamily: {
       daumBold: ['WooridaumB'],
       daumLight: ['WooridaumL'],
+    },
+    colors: {
+      ...colors,
+      wooriYellow: "#f1c40f", // 강조용
     },
   },
   plugins: [],
