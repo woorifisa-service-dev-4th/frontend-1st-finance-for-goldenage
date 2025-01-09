35108917000ebc85da5f95581997d860d9025a4f
namsh1125 <namsh1125@naver.com>
2025-01-09 14:36:11 +0900

    🎉더미 데이터 추가

---
+++ db/loans.js
@@ -0,0 +1,24 @@
+const loans = [
+    {
+        id: 1,
+        title: '우리 주거래 직장인대출(인터넷뱅킹)',
+        interest: '연 4.5%~',
+        limit: 150000000,
+        maxTerm: 84,
+        condition: '직장인',
+        link: 'https://spot.wooribank.com/pot/Dream?withyou=POLON0052&cc=c010528:c010531;c012425:c012399&PLM_PDCD=P020000054&PRD_CD=P020000054&HOST_PRD_CD=2001122111184',
+        category: 'loan'
+    },
+    {
+        id: 2,
+        title: '우리 사잇돌 중금리대출',
+        interest: '연 5.9%~',
+        limit: 20000000,
+        maxTerm: 60,
+        condition: '직장인',
+        link: 'https://spot.wooribank.com/pot/Dream?withyou=POLON0052&cc=c010528:c010531;c012425:c012399&PLM_PDCD=P020000004&PRD_CD=P020000004&HOST_PRD_CD=2011113001184',
+        category: 'loan'
+    }
+];
+
+export default loans;
